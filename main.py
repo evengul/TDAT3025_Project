d3c399df20131a21d05dca4201f3eb7a0c715e12
evengu <evengu@stud.ntnu.no>
2020-11-24 10:51:12 +0100

    Method 1.1

--- main.py
+++ main.py
@@ -68,34 +68,33 @@ class AML:
     def f(self, x, c_0, p_0):
         c_1, p_1 = c_0, p_0
         allIs = list(range(len(self.noise.noise)))
-        flipped = False     # Have we misclassified this image?
+        np.random.shuffle(allIs)
+        flipped = False  # Have we misclassified this image?
         # For all tiles in the noise
         for i in allIs:
             for j in allIs:
                 q = random.choice(self.Q)  # Choose a vector to create the difference along (R, G or B)
                 # For every direction on this vector, with the size of the learning rate
-                for a in [self.learning_rate, -self.learning_rate]:
-                    # Make a copy of the noise
-                    possibly_changing_noise = self.noise.noise.detach().clone()
-                    # Change the noise copy with the randomly chosen vector in direction a
-                    possibly_changing_noise[i][j][0] += a * q[0]
-                    possibly_changing_noise[i][j][1] += a * q[1]
-                    possibly_changing_noise[i][j][2] += a * q[2]
-                    # Get the new class and probability of the direction along the chosen vector
-                    x += self.noise.to_image_tensor('cuda', noise=possibly_changing_noise)
-                    c_1, p_1 = self.internal_model.run(x)
-                    # If we haven't flipped yet, and do so now, we can flip
-                    if not flipped and c_1 != c_0:
-                        flipped = True
-                    # If we have flipped and still are flipped, and the percentage is larger than it was before,
-                    # perform the change.
-                    if flipped and c_1 != c_0 and p_1.item() > p_0.item():
-                        self.noise.noise = possibly_changing_noise
-                        break
-                    # Otherwise, try the other way on this vector.
-                    # If that doesn't work, we choose a new random vector
-                # Smoothen the noise
-                self.noise.noise = torch.tanh_(self.noise.noise)
+                a = self.learning_rate
+                # Make a copy of the noise
+                possibly_changing_noise1 = self.noise.noise.detach().clone()
+                possibly_changing_noise2 = self.noise.noise.detach().clone()
+                # Change the noise copy with the randomly chosen vector in direction a
+                possibly_changing_noise1[i][j][0] += a * q[0]
+                possibly_changing_noise1[i][j][1] += a * q[1]
+                possibly_changing_noise1[i][j][2] += a * q[2]
+
+                possibly_changing_noise2[i][j][0] -= a * q[0]
+                possibly_changing_noise2[i][j][1] -= a * q[1]
+                possibly_changing_noise2[i][j][2] -= a * q[2]
+                positive = x + self.noise.to_image_tensor('cuda', noise=possibly_changing_noise1)
+                negative = x + self.noise.to_image_tensor('cuda', noise=possibly_changing_noise2)
+                c11, p11 = self.internal_model.run(positive)
+                c12, p12 = self.internal_model.run(negative)
+                if p11 > p12:
+                    self.noise.noise = possibly_changing_noise1
+                else:
+                    self.noise.noise = possibly_changing_noise2
         return c_1, p_1
 
     # Accuracy
@@ -110,7 +109,7 @@ class AML:
             i += 1
             img = get_x(path)
             c0, p0, c1, p1, x = self.apply_changes(img, eps)
-            if c0 != c1 and p1 >= 0.85 * p0 or p1 <= 0.1 * p0:  # or p1 <= x * p0
+            if c0 != c1 and p1 >= 0.85 * p0:  # or p1 <= x * p0
                 correct += 1.0
         return correct / total
 
@@ -118,7 +117,7 @@ class AML:
     def train(self, n_runs=50, n_img=500):
         i = 0
         # files = random.sample(self.x_paths_train.tolist(), n_img) # Select random images to train on
-        files = self.x_paths_train[0: n_img]    # Select images from the same class to train on
+        files = self.x_paths_train[0: n_img]  # Select images from the same class to train on
 
         start_time = time.time()
 
@@ -219,6 +218,13 @@ def run_personal(path, model, eps):
     print("-----------------")
 
 
+def run_personal2(path, model, eps):
+    img_obj = PersonalImage(path)
+    x = img_obj.x.cpu()
+    c_true, p_true, c_adv, p_adv, x = model.apply_changes(x, eps)
+    return c_true, p_true, c_adv, p_adv
+
+
 # Make a gif of all images in the folder containing images produced by run_personal
 def make_gif(name):
     out_path = "images/gifs/%s.gif" % name
@@ -234,34 +240,43 @@ if torch.cuda.is_available():
     Q = Q.to(device='cuda')
 
 # The model we want to train on, loaded with preexisting noise
-model = AML(retrieve_train_paths(), retrieve_test_paths(), Q, 0.1, 7, "noise.pt")
+model = AML(retrieve_train_paths(), retrieve_test_paths(), Q, 0.1, 7)
 
 # Train the model
-model.train(n_runs=1, n_img=10)
+model.train(n_runs=2, n_img=40)
 model.noise.save_noise('RGB')
-model.noise.get_stats()
+# model.noise.get_stats()
 
 # 0.4 is chosen from testing the code below
-print(model.accuracy(0.4))
+# print(model.accuracy(0.04))
 
 # Uncomment to see the change in percentages for various eps-values
 # for eps in range(1, 10):
 #     eps = eps / 10000
 #     run_personal("images/personal/dog.jpg", model, eps)
-#     run_personal(hourglassPath, model, eps)
 #
 # for eps in range(1, 10):
 #     eps = eps / 1000
 #     run_personal("images/personal/dog.jpg", model, eps)
-#     run_personal(hourglassPath, model, eps)
 #
 # for eps in range(1, 10):
 #     eps = eps / 100
 #     run_personal("images/personal/dog.jpg", model, eps)
-#     run_personal(hourglassPath, model, eps)
 #
 # for eps in range(1, 10):
 #     eps = eps / 10
 #     run_personal("images/personal/dog.jpg", model, eps)
-#     run_personal(hourglassPath, model, eps)
-
+labels = load_labels()
+highest_prob = 0
+highest_prob_eps = 0
+runs = 500
+image = "dog.jpg"
+for eps in range(1, runs):
+    if eps == runs - 1:
+        run_personal("images/personal/" + image, model, highest_prob_eps)
+    eps = eps / 1000
+    c_true, p_true, c_adv, p_adv = run_personal2("images/personal/" + image, model, eps)
+    print("eps", eps, "class", labels[str(c_adv)], "{:.3f}".format(p_adv.item() * 100) + "% probability")
+    if p_adv.item() > highest_prob and c_adv != c_true:
+        highest_prob = p_adv.item()
+        highest_prob_eps = eps
